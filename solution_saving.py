d504629df8bc357e163f5253f223176fa513ab59
Heet Sakaria <heet9022@users.noreply.github.com>
2024-02-28 13:40:31 -0500

    reduce mean cost by removing stop condition

--- solution_saving.py
+++ solution_saving.py
@@ -29,71 +29,72 @@ class Solution:
                     savings.append(saving)
 
         savings = sorted(savings, key = lambda x: x[1], reverse=True)
-
+        
         for link, saving in savings:
 
-            # If not all loads are assigned
-            if len(self.assigned) != len(self.loadByID):
+            load1 = self.loadByID[link[0]]
+            load2 = self.loadByID[link[1]]
 
-                # condition a. Either, neither i nor j have already been assigned to a route, 
-                # ...in which case a new route is initiated including both i and j.
-                if (link[0] not in self.assigned) and (link[1] not in self.assigned):
+            # condition a. Either, neither i nor j have already been assigned to a route, 
+            # ...in which case a new route is initiated including both i and j.
+            if (link[0] not in self.assigned) and (link[1] not in self.assigned):
 
-                    # calculate distance
-                    cost = self.computeDistance([self.loadByID[link[0]], self.loadByID[link[1]]], True, True)
+                # calculate distance
+                cost = self.computeDistance([load1, load2], True, True)
+                if cost <= self.max_distance:
+                    d = util.Driver()
+                    d.route = [load1, load2]
+                    self.drivers.append(d)
+                    self.assigned[link[0]] = d
+                    self.assigned[link[1]] = d
+
+            # condition b. Or, exactly one of the two nodes (i or j) has already been included 
+            # ...in an existing route and that point is not interior to that route 
+            # ...(a point is interior to a route if it is not adjacent to the depot D in the order of traversal of nodes), 
+            # ...in which case the link (i, j) is added to that same route.    
+            elif (link[0] in self.assigned) and (link[1] not in self.assigned):
+
+                d = self.assigned[link[0]]
+                i = d.route.index(load1)
+                # if node is the last node of route
+                if i == len(d.route) - 1:
+                    cost = self.computeDistance(d.route + [load2], True, True)
                     if cost <= self.max_distance:
-                        d = util.Driver()
-                        d.route = [self.loadByID[link[0]], self.loadByID[link[1]]]
-                        self.drivers.append(d)
-                        self.assigned[link[0]] = d
+                        d.route.append(load2)
                         self.assigned[link[1]] = d
 
-                # condition b. Or, exactly one of the two nodes (i or j) has already been included 
-                # ...in an existing route and that point is not interior to that route 
-                # ...(a point is interior to a route if it is not adjacent to the depot D in the order of traversal of nodes), 
-                # ...in which case the link (i, j) is added to that same route.    
-                elif (link[0] in self.assigned) and (link[1] not in self.assigned):
+            elif (link[0] not in self.assigned) and (link[1] in self.assigned):
 
-                    d = self.assigned[link[0]]
-                    i = d.route.index(self.loadByID[link[0]])
-                    # if node is the last node of route
-                    if i == len(d.route) - 1:
-                        cost = self.computeDistance(d.route + [self.loadByID[link[1]]], True, True)
-                        if cost <= self.max_distance:
-                            d.route.append(self.loadByID[link[1]])
-                            self.assigned[link[1]] = d
- 
-                elif (link[0] not in self.assigned) and (link[1] in self.assigned):
+                d = self.assigned[link[1]]
+                i = d.route.index(load2)
+                # if node is the first node of route
+                if i == 0:
+                    cost = self.computeDistance([load1] + d.route, True, True)
+                    if cost <= self.max_distance:
+                        d.route = [load1] + d.route
+                        self.assigned[link[0]] = d
 
-                    d = self.assigned[link[1]]
-                    i = d.route.index(self.loadByID[link[1]])
-                    # if node is the first node of route
-                    if i == 0:
-                        cost = self.computeDistance([self.loadByID[link[0]]] + d.route, True, True)
-                        if cost <= self.max_distance:
-                            d.route = [self.loadByID[link[0]]] + d.route
-                            self.assigned[link[0]] = d
+            # condition c. Or, both i and j have already been included in two different existing routes 
+            # ...and neither point is interior to its route, in which case the two routes are merged.        
+            else:
 
-                # condition c. Or, both i and j have already been included in two different existing routes 
-                # ...and neither point is interior to its route, in which case the two routes are merged.        
-                else:
+                d1 = self.assigned[link[0]]
+                i1 = d1.route.index(load1)
 
-                    d1 = self.assigned[link[0]]
-                    i1 = d1.route.index(self.loadByID[link[0]])
+                d2 = self.assigned[link[1]]
+                i2 = d2.route.index(load2)
 
-                    d2 = self.assigned[link[1]]
-                    i2 = d2.route.index(self.loadByID[link[1]])
-
-                    # if node1 is the last node of its route and node 2 is the first node of its route and the routes are different
-                    if (i1 == len(d.route) - 1) and (i2 == 0) and (d1 != d2):
-                        cost = self.computeDistance(d1.route + d2.route, True, True)
-                        if cost <= self.max_distance:
-                            d1.route = d1.route + d2.route
-                            for load in d2.route:
-                                self.assigned[int(load.id)] = d1
-                            
-                            self.drivers.remove(d2)
+                # if node1 is the last node of its route and node 2 is the first node of its route and the routes are different
+                if (i1 == len(d.route) - 1) and (i2 == 0) and (d1 != d2):
+                    cost = self.computeDistance(d1.route + d2.route, True, True)
+                    if cost <= self.max_distance:
+                        d1.route = d1.route + d2.route
+                        for load in d2.route:
+                            self.assigned[int(load.id)] = d1
                         
+                        self.drivers.remove(d2)
+
+        # Assign all unassigned drivers to individual routes      
         for i in range(1, len(self.loadByID) + 1):
             if i not in self.assigned:
                 d = util.Driver(0, [])
@@ -122,7 +123,7 @@ class Solution:
         for d in self.drivers:
             lids =[]
             for load in d.route:
-                lids.append(load.id)
+                lids.append(int(load.id))
             print(lids)
 
                 
